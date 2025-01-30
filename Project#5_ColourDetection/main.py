85f3a704d85635e1074b28c81a7882ae9b4b7c71
Tushar Goyal <tushar.goyalbbms@gmail.com>
2025-01-31 02:45:33 +0530

    slight modifications

--- Project#5_ColourDetection/main.py
+++ Project#5_ColourDetection/main.py
@@ -1,23 +1,60 @@
 import cv2 as cv
 import numpy as np
+import cvzone
 
 capture=cv.VideoCapture(0)
+capture.set(cv.CAP_PROP_BRIGHTNESS,0.5)
 
-lowerOrange=np.array([11,195,90])
-upperOrange=np.array([18,255,157])
+#testColours
+lowerOrange=np.array([7,66,255])
+upperOrange=np.array([22,97,255])
 upper_PurplePen=np.array([124,169,126])
 lower_PurplePen=np.array([118,132,77])
+cap_lower=np.array([105,46,16])
+cap_upper=np.array([130,179,40])
 upper_face=np.array([15,133,211])
 lower_face=np.array([4,36,64])
-
-
-
+minRed=np.array([0,152,226])
+maxBlue=np.array([101,255,255])
+minBlue=np.array([95,191,254])
+maxRed=np.array([179,245,255])
+BGRorange=(100,165,255)
+BGRskinColor=(90,100,140)
+BGR_BLACK=(0,0,0)
 maxh=0
 maxs=0
 maxv=0
 minh=255
 mins=255
 minv=255
+
+
+
+
+def detect_color(mask,colorCode,ColorName,mode):
+    contour,heir= cv.findContours(mask,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE)
+    if len(contour)!=0:
+        for i in contour:
+            area=cv.contourArea(i)
+            peri=cv.arcLength(i,True)
+            if (area >3000) and peri>10:
+                x,y,w,h=cv.boundingRect(i)
+                # aprox=cv.approxPolyDP(i,0.07*peri,True)
+                if mode=="rect":
+
+                    cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+                    cv.circle(frame,(x,y),5,(255,255,255),cv.FILLED)
+                    cv.circle(frame,(x+w,y),5,(255,255,255),cv.FILLED)
+                    cv.circle(frame,(x,y+h),5,(255,255,255),cv.FILLED)
+                    cv.circle(frame,(x+w,y+h),5,(255,255,255),cv.FILLED)
+                    cvzone.putTextRect(frame,f'{ColorName}',(x+w//4,y-30),2,2,(255,255,255),colorCode)
+
+                if mode=="point":
+                    cvzone.putTextRect(frame,f'{ColorName}',(x+w//2,y-30+h//2),2,2,(255,255,255),colorCode)
+                    cv.circle(frame,(x+w//2,y+h//2),8,colorCode,cv.FILLED)
+
+
+
 while True: 
 
 
@@ -28,11 +65,10 @@ while True:
     hsvimg=cv.cvtColor(frame,cv.COLOR_BGR2HSV)
     cv.circle(frame,(cx,cy),5,(255,0,0),3)
     pixcel_center=frame[cy,cx]
+    # print(pixcel_center)
     bgr= pixcel_center.reshape(1,1,3)
     hsvpixcel=cv.cvtColor(bgr,cv.COLOR_BGR2HSV)
-    
 
-    # print("h",hsvpixcel[0][0][0])
     if maxh<hsvpixcel[0][0][0]:
         maxh=hsvpixcel[0][0][0]
     if maxs<hsvpixcel[0][0][1]:
@@ -45,35 +81,8 @@ while True:
         mins=hsvpixcel[0][0][1]
     if minv>hsvpixcel[0][0][2]:
         minv=hsvpixcel[0][0][2]
-    
-
     print(f'max(hsv)= {maxh},{maxs},{maxv}')
     print(f'min(hsv)= {minh},{mins},{minv}')
-
-    
-    mask=cv.inRange(hsvimg,lower_face,upper_face)
-    # face("hsv",hsvimg)
-    cv.imshow("mask",mask)
-    contour,heir= cv.findContours(mask,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE)
-    bigC=np.array([[0,0],[100,0],[100,100],[0,100]])
-    if len(contour)!=0:
-        for i in contour:
-            area=cv.contourArea(i)
-            peri=cv.arcLength(i,True)
-            if (area >1000) and peri>10:
-                x,y,w,h=cv.boundingRect(i)
-                # aprox=cv.approxPolyDP(i,0.07*peri,True)
-                cv.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),1)
-    #             if (len(aprox)==4):
-    #                 cv.polylines(frame,[aprox],True,(0,255,0),1)
-
-
-    
-    # contour,hairarhcie= cv.findContours(mask,)
-    cv.imshow("Img",frame)
-
-    if cv.waitKey(1) & 0XFF==27:
-        break
     if cv.waitKey(1) & 0xFF == ord('q'):
         maxh=0
         maxs=0
@@ -81,7 +90,24 @@ while True:
         minh=255
         mins=255
         minv=255
-        cv.waitKey(20)
+        
+    
+    # mask1=cv.inRange(hsvimg,lower_face,upper_face)
+    # mask2=cv.inRange(hsvimg,lowerOrange,upperOrange)
+    mask3=cv.inRange(hsvimg,minRed,maxRed)
+    mask4=cv.inRange(hsvimg,minBlue,maxBlue)
+    # cv.imshow("hsv",cv.resize(hsvimg,(400,300)))
+    cv.imshow("mask",cv.resize(mask4,(400,300)))
+    # # cv.imshow("mask2",mask2)
+
+    # detect_color(mask1,BGRskinColor,"skin",mode="rect")
+    # detect_color(mask2,BGRorange,"orange",mode="rect")
+
+    cv.imshow("Img",frame)
+
+    if cv.waitKey(1) & 0XFF==27:
+        break
+
 capture.release()
 cv.destroyAllWindows()
 
