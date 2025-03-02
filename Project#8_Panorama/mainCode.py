69d506b623f73ea0fb2d9f4ede316703f29dbc35
Tushar Goyal <tushar.goyalbbms@gmail.com>
2025-03-02 19:22:25 +0530

    made a bit changes, with save featurepython -u

--- Project#8_Panorama/mainCode.py
+++ Project#8_Panorama/mainCode.py
@@ -3,31 +3,43 @@ import numpy as np
 import os
 
 MF='Resource'
+save_Location='Output'
 myF=os.listdir(MF)
 
+
 for folder in myF:
-    path=MF+'/'+folder
-    list=os.listdir(path)
-    imageArray=[]
-    i=0
-    for images in list:
-         
-         imgread= cv.imread(f'{path}/{images}')
-         imgread=cv.resize(imgread,(500,500))
-         imageArray.append(imgread)
-        
-         cv.imshow(f'{i}',imageArray[i])
-         cv.imshow
-         i+=1
-    print(f'{len(imageArray)} image(s) found')
-    stitcher=cv.Stitcher.create()
-    (status,result)=stitcher.stitch(imageArray)
-    if status==cv.STITCHER_OK:
-         print("generate done")
-         cv.imshow(folder+' result',result)
-         cv.waitKey(1)
-    else:
-         print("not generated")
+
+     path=MF+'/'+folder
+     list=os.listdir(path)
+     imageArray=[]
+     i=0
+     for images in list:
+
+          imgread= cv.imread(f'{path}/{images}')
+          imgread=cv.resize(imgread,(500,500))
+          imageArray.append(imgread)
+          cv.imshow(f'{i}',imageArray[i])
+          cv.imshow
+          i+=1
+     print(f'{len(imageArray)} image(s) found')
+     stitcher=cv.Stitcher.create()
+     (status,result)=stitcher.stitch(imageArray)
+     if status==cv.STITCHER_OK:
+          print("generate done")
+          cv.imshow(folder+' result',result)
+          cv.waitKey(1)
+     else:
+          print("not generated")
+     cv.waitKey(0)
+     cv.destroyAllWindows()
+     # print(str('Output/'+folder+'_result'+'.jpg'))
+     save=int (input("wanna save?"))
+     if save ==1:   
+          cv.imwrite(str(save_Location+folder+'/_result'+'.jpg'),result)
+          print("result saved")
+
+
+
     # print(list)
 # img=cv.imread(f'{path}')
 # cv.imshow("img",img)
